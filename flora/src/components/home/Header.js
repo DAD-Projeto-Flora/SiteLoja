7744b48fa8d27bfd6fff95ff648608fd6149b2ea
ciintyaarioss <157593135+ciintyaarioss@users.noreply.github.com>
2025-04-09 08:41:00 -0300

    Update Header.js

--- flora/src/components/home/Header.js
+++ flora/src/components/home/Header.js
@@ -32,7 +32,6 @@ const Header = () => {
 
           <ul className="nav-links">
             <li><Link to="/about">Sobre a Flora</Link></li>
-            <li><Link to="/pages/product/product.js">Parceiros</Link></li>
           </ul>
 
           <form className="search-form" onSubmit={handleSearchSubmit}>
