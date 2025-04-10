733e2174396d1597da55074a3b036ef70b9f6242
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-10 09:50:46 -0300

    refatorando o componente ProductCard para melhorar a legibilidade e organização do código

--- flora/src/components/catalog/ProductCard.js
+++ flora/src/components/catalog/ProductCard.js
@@ -1,41 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Importa o Link
 import "./ProductCard.css";
 
 const ProductCard = ({ produto }) => {
   const renderStars = (avaliacao) => {
     const totalEstrelas = 5;
-    const estrelasCheias = Math.floor(avaliacao); // Número inteiro de estrelas cheias
-    const temMeiaEstrela = avaliacao % 1 !== 0; // Verifica se há parte decimal
+    const estrelasCheias = Math.floor(avaliacao);
+    const temMeiaEstrela = avaliacao % 1 !== 0;
     const estrelasVazias = totalEstrelas - estrelasCheias - (temMeiaEstrela ? 1 : 0); 
-  
+
     return (
       <div className="estrelas">
         {Array.from({ length: estrelasCheias }, (_, index) => (
-          <span key={`cheia-${index}`} className="estrela cheia">
-            ★
-          </span>
+          <span key={`cheia-${index}`} className="estrela cheia">★</span>
         ))}
         {temMeiaEstrela && (
-          <span className="estrela meia">
-            ★
-          </span>
+          <span className="estrela meia">★</span>
         )}
         {Array.from({ length: estrelasVazias }, (_, index) => (
-          <span key={`vazia-${index}`} className="estrela vazia">
-            ★
-          </span>
+          <span key={`vazia-${index}`} className="estrela vazia">★</span>
         ))}
       </div>
     );
   };
 
   return (
-    <div className="produto-card">
-      <div className="img-produto"><img src={produto.imagem} alt={produto.nome} /></div>
-      <h3>{produto.nome}</h3>
-      {renderStars(produto.avaliacao)}
-      <p>R$ {produto.preco}</p>
-    </div>
+    <Link to={`/productpage/${produto.id}`} className="link-sem-estilo"> 
+      <div className="produto-card">
+        <div className="img-produto">
+          <img src={produto.imagem} alt={produto.nome} />
+        </div>
+        <h3>{produto.nome}</h3>
+        {renderStars(produto.avaliacao)}
+        <p>R$ {produto.preco}</p>
+      </div>
+    </Link>
   );
 };
 
