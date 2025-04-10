dbe3c19288389355f1e70c20594ac61358757689
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-10 09:47:20 -0300

    implementando lógica para buscar produto por ID e exibindo informações dinâmicas

--- flora/src/pages/product/Product.js
+++ flora/src/pages/product/Product.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Header from '../../components/home/Header';
 import Footer from '../../components/home/Footer';
 import ProductPage from '../../components/productpage/ProductPage';
 
-
-
 const Product = () => {
-    return (
-        <div>
-        <Header />
-        <main>
-            <ProductPage 
-            image="https://www.w3schools.com/howto/img_avatar2.png"
-            title="Maria Joaquina da Silva"
-            price="R$ 100,00"
-            desc= "adawdawdadawdawd"
-            rating={4}
-            category="Roupas"/>
-        </main>
-        <Footer/>
-        </div>
-    );
-}
+  const { id } = useParams();
+  const [produto, setProduto] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProduto = async () => {
+      try {
+        const response = await fetch(`/api/getProductById?id=${id}`);
+        const data = await response.json();
+        setProduto(data);
+      } catch (error) {
+        console.error("Erro ao buscar produto", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduto();
+  }, [id]);
+
+  if (loading) return <p>Carregando...</p>;
+  if (!produto) return <p>Produto não encontrado</p>;
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <ProductPage 
+          image={produto.urlImagem}
+          title={produto.nome}
+          price={`R$ ${produto.precoUnid.toFixed(2).replace(".", ",")}`}
+          desc={produto.descricao}
+          rating={produto.notaAvaliacao}
+          category={produto.categoria}
+        />
+      </main>
+      <Footer/>
+    </div>
+  );
+};
 
 export default Product;
