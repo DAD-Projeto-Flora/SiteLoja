47d0ea333615fb47c56d3a0fb5d99affd1d4fda9
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-11 08:37:07 -0300

    adicionando lógica de logout e ajustando a estrutura do componente ConfigCard

--- flora/src/components/config/ConfigCard.js
+++ flora/src/components/config/ConfigCard.js
@@ -1,19 +1,26 @@
 import React from "react";
-import "./ConfigCard.css"; // Importa o arquivo de estilos
+import "./ConfigCard.css";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../login/UserContext"; // ajuste o caminho conforme necessário
 
-const ConfigCard = ({name,image, email, password}) => {
+const ConfigCard = ({ name, image, email, password }) => {
+  const { setTipoUsuario } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setTipoUsuario(null);
+    localStorage.removeItem("tipoUsuario");
+    navigate("/login");
+  };
+
   return (
     <div className="profile-container">
       <div className="header-decoration"></div>
-      
+
       <div className="profile-card">
         <div className="header-profile">
           <div className="user-info">
-            <img
-              className="avatar"
-              src={image}
-              alt="Foto de Perfil"
-            />
+            <img className="avatar" src={image} alt="Foto de Perfil" />
             <div>
               <h2 className="text">{name}</h2>
               <p className="text">{email}</p>
@@ -27,42 +34,42 @@ const ConfigCard = ({name,image, email, password}) => {
             <div>
               <div>
                 <label className="text">Senha antiga</label>
-                <input type="text" placeholder="Insira sua senha atual" className="input-config-card"/>
+                <input type="text" placeholder="Insira sua senha atual" className="input-config-card" />
               </div>
               <div>
                 <label className="text">Confirme a nova senha</label>
-                <input type="text" placeholder="Insira sua nova senha novamente" className="input-config-card"/>
+                <input type="text" placeholder="Insira sua nova senha novamente" className="input-config-card" />
               </div>
             </div>
           </div>
-          
         </div>
-          
+
         <div className="form-grid">
           <div className="info-grid">
             <div>
               <div>
                 <label className="text">Nova senha</label>
-                <input type="text" placeholder="Insira sua nova senha" className="input-config-card"/>
+                <input type="text" placeholder="Insira sua nova senha" className="input-config-card" />
               </div>
               <div>
                 <label className="text">Mudar e-mail</label>
-                <input type="text" placeholder="Insira seu novo e-mail" className="input-config-card"/>
+                <input type="text" placeholder="Insira seu novo e-mail" className="input-config-card" />
               </div>
             </div>
             <div className="delete-part">
-            <div>
               <div className="delete">
                 <label className="text">Excluir conta</label>
                 <button className="delete-button">Deletar conta</button>
               </div>
+              <div className="logout">
+                <label className="text">Sair da conta</label>
+                <button className="logout-button" onClick={handleLogout}>Deslogar</button>
+              </div>
             </div>
           </div>
-          </div>
-          
         </div>
-          </div>
-        </div>
+      </div>
+    </div>
   );
 };
 
