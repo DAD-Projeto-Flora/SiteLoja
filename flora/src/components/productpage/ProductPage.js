a287a3c108f6f8f698a292ba80904740794766ce
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-13 03:16:06 -0300

    corrige a exibição das informações do produto na ProductPage

--- flora/src/components/productpage/ProductPage.js
+++ flora/src/components/productpage/ProductPage.js
@@ -41,18 +41,29 @@ const ProductPage = () => {
   return (
     <div className="product-page">
       <div className="product-content">
-        <img src={produto.imagem} alt={produto.nome} className="product-image-page" />
+        <img src={produto.urlImagem} alt={produto.nome} className="product-image-page" />
         <div className="product-details">
           <h2 className="text title-productpage">{produto.nome}</h2>
           <p className="rating">
             {Array.from({ length: 5 }, (_, index) => (
-              <span key={index} className={index < produto.avaliacao ? "star filled" : "star"}>★</span>
+              <span
+                key={index}
+                className={index < Math.round(produto.notaAvaliacao) ? "star filled" : "star"}
+              >
+                ★
+              </span>
             ))}
           </p>
-          <p className="price-productpage text">R$ {produto.preco}</p>
-          <p className="category text"><span className="negrito">Categoria: </span>{produto.categoria}</p>
-          <p className="desc text"><span className="negrito">Descrição: </span>{produto.descricao}</p>
-          <button className="add-to-cart">Adicionar ao carrinho <span><img src="/carrinho.svg" alt="Flora Logo Folha" /></span></button>
+          <p className="price-productpage text">R$ {produto.precoUnid.toFixed(2)}</p>
+          <p className="category text">
+            <span className="negrito">Categoria: </span>{produto.categoria.nome}
+          </p>
+          <p className="desc text">
+            <span className="negrito">Descrição: </span>{produto.descricao || "Sem descrição disponível."}
+          </p>
+          <button className="add-to-cart">
+            Adicionar ao carrinho <span><img src="/carrinho.svg" alt="Flora Logo Folha" /></span>
+          </button>
         </div>
       </div>
     </div>
