5cfae6a969bf32ab4ff09209661db662cda8329f
Brenosilva13 <breno.souza@germinare.org.br>
2025-04-12 23:21:45 -0300

    atualiza WalletCard para exibir corretamente os dados do cartão e melhora a formatação dos campos

--- flora/src/components/wallet/WalletCard.js
+++ flora/src/components/wallet/WalletCard.js
@@ -4,39 +4,38 @@ import { useUser } from "../login/UserContext";
 import { getCardByClientId } from "../../autenticação/getCardByClientId";
 
 const WalletCard = () => {
-   const [card, setCard] = useState(null);
-    const { userId } = useUser();
-  
-    useEffect(() => {
-      const fetchClient = async () => {
-        try {
-          const data = await getCardByClientId(userId);
-          console.log(data)
-          setCard(data);
-        } catch (error) {
-          console.error("Erro ao carregar cliente:", error);
-        }
-      };
-  
-      if (userId) fetchClient();
-    }, [userId]);
-  
+  const [card, setCard] = useState(null);
+  const { userId } = useUser();
+
+  useEffect(() => {
+    const fetchClient = async () => {
+      try {
+        const data = await getCardByClientId(userId);
+        console.log(data);
+        setCard(data);
+      } catch (error) {
+        console.error("Erro ao carregar cliente:", error);
+      }
+    };
+
+    if (userId) fetchClient();
+  }, [userId]);
+
+  if (!card) {
+    return <p>Carregando cartão...</p>;
+  }
 
   return (
     <div className="profile-container">
       <div className="header-decoration"></div>
-      
+
       <div className="profile-card">
         <div className="header-profile">
           <div className="user-info">
-            <img
-              className="avatar"
-              src={"/perfil.svg"}
-              alt="Foto de Perfil"
-            />
+            <img className="avatar" src={"/perfil.svg"} alt="Foto de Perfil" />
             <div>
-              <h2 className="text">{}</h2>
-              <p className="text">{}</p>
+              <h2 className="text">{card.nomeCartao}</h2>
+              <p className="text">•••• •••• •••• {card.numero?.slice(-4)}</p>
             </div>
           </div>
           <button className="save-button">Salvar</button>
@@ -47,50 +46,73 @@ const WalletCard = () => {
             <div>
               <div>
                 <label className="text">Número do cartão</label>
-                <input type="text" placeholder={card.numero} className="input-wallet-card"/>
+                <input
+                  type="text"
+                  placeholder={card.numero}
+                  className="input-wallet-card"
+                />
               </div>
               <div>
                 <label className="text">Data de Validade (MMAA)</label>
-                <input type="text" placeholder={card.validade} className="input-wallet-card"/>
+                <input
+                  type="text"
+                  placeholder={
+                    card.validade
+                      ? new Date(card.validade).toLocaleDateString("pt-BR", {
+                          month: "2-digit",
+                          year: "2-digit",
+                        })
+                      : ""
+                  }
+                  className="input-wallet-card"
+                />
               </div>
             </div>
           </div>
-          
         </div>
-          
+
         <div className="form-grid">
           <div className="name-grid">
             <div>
               <div>
                 <label className="text">Nome do cartão</label>
-                <input type="text" placeholder={card.nomeCartao} className="input-wallet-card"/>
+                <input
+                  type="text"
+                  placeholder={card.nomeCartao}
+                  className="input-wallet-card"
+                />
               </div>
               <div>
                 <label className="text">CVV</label>
-                <input type="text" placeholder={card.cvv} className="input-wallet-card"/>
+                <input
+                  type="text"
+                  placeholder={card.cvv}
+                  className="input-wallet-card"
+                />
               </div>
             </div>
             <div className="info-card">
-            <div>
-              <div className="card-info">
-                <label className="text">Cartão cadastrado</label>
-                <div className="card-box">
-                  <span>
-                    <img className="icon"
-                    src="/logoCartao.svg"
-                    alt="Endereço"/>
-                  </span>
-                  <p className="text">{card.nomeCartao}</p>
+              <div>
+                <div className="card-info">
+                  <label className="text">Cartão cadastrado</label>
+                  <div className="card-box">
+                    <span>
+                      <img
+                        className="icon"
+                        src="/logoCartao.svg"
+                        alt="Endereço"
+                      />
+                    </span>
+                    <p className="text">{card.nomeCartao}</p>
+                  </div>
+                  <button className="add-button">Atualizar cartão</button>
                 </div>
-                <button className="add-button">Atualizar cartão</button>
               </div>
             </div>
           </div>
-          </div>
-          
         </div>
-          </div>
-        </div>
+      </div>
+    </div>
   );
 };
 
